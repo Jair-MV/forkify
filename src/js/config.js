8f31485d57c996fb7b290375cf8385dbef226392
Jair-MV <145881572+Jair-MV@users.noreply.github.com>
2024-10-29 16:51:59 -0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c1a4c915-4c3e-4227-826c-a521f50c7ec8';
+export const MODAL_CLOSE_SEC = 2.5;
